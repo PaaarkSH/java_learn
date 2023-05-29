3784eb879210e91f38b62c3d9a317eaebb9be998
psh950 <psh950@naver.com>
2023-05-29 23:34:53 +0900

    자바에서 제공하는 함수형 인터페이스

--- jump_to_java/Test.java
+++
@@ -1,8 +0,0 @@
-package jump_to_java;
-
-public class Test {
-    public static void main(String[] args){
-        if ("7" > "17")
-            System.out.println();
-    }
-}
--- thejava8/section1/Foo.java
+++ thejava8/section1/Foo.java
@@ -1,9 +1,33 @@
 package thejava8.section1;
 
+import java.lang.FunctionalInterface;
+import java.util.function.*;
+
 public class Foo {
     public static void main(String[] args) {
         int base = 10;
-    RunSomething runSomething = (number) -> number + base;
-        System.out.println(runSomething.doIt(1));
+        // RunSomething runSomething = (number) -> number + base;
+        // System.out.println(runSomething.doIt(1));
+
+        // Plus10 plus10 = new Plus10();
+        // System.out.println(plus10.apply(1));
+
+        Function<Integer, Integer> plus10 = (i) -> i + 10;
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+
+        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
+        System.out.println(multiply2AndPlus10.apply(2));
+
+        System.out.println(plus10.andThen(multiply2).apply(2));
+
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(10);
+
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println(get10.get());
+
+        Predicate<String> stringPredicate = (s) -> s.startsWith("keesun");
+
+        UnaryOperator<Integer> plus10New = (i) -> i + 10;
     }
 }
