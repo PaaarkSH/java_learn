7e08c652a8f04ddfed8d0c9b417fe490da641a43
박상훈 <psh950@naver.com>
2023-06-08 20:48:43 +0900

    스트림 하다만거

--- thejava8/section2/App.java
+++ thejava8/section2/App.java
@@ -6,26 +6,35 @@ import java.util.stream.*;
 public class App {
 
     public static void main(String[] args) {
-        List<String> name = new ArrayList<>();
-        name.add("park");
-        name.add("kin");
-        name.add("lee");
-        name.add("choi");
+        List<String> names = new ArrayList<>();
+        names.add("park");
+        names.add("kin");
+        names.add("lee");
+        names.add("choi");
 
-        // name.forEach(System.out::println);
+        // names.forEach(System.out::println);
 
-//        Spliterator<String> spliterator = name.spliterator();
+//        Spliterator<String> spliterator = names.spliterator();
 //        Spliterator<String> spliterator1 = spliterator.trySplit();
 //        while(spliterator.tryAdvance(System.out::println));
 //        System.out.println("---------------------------------");
 //        while(spliterator1.tryAdvance(System.out::println));
 
-//        name.removeIf(s -> s.startsWith("k"));
-//        name.forEach(System.out::println);
-        // name.sort(String::compareToIgnoreCase);
-        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
-        name.sort(compareToIgnoreCase);
-        name.sort(compareToIgnoreCase.reversed());
+//        names.removeIf(s -> s.startsWith("k"));
+//        names.forEach(System.out::println);
+        // names.sort(String::compareToIgnoreCase);
+//        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+//        names.sort(compareToIgnoreCase);
+//        names.sort(compareToIgnoreCase.reversed());
+
+//        Stream<String> stringStream = names.stream().map(String::toUpperCase);
+//        names.forEach(System.out::println);
+        names.stream().map(s ->{
+            System.out.println(s);
+            return s.toUpperCase();
+        });
+        System.out.println("---------------------------------");
+        names.forEach(System.out::println);
     }
 
 }
