634d163d98c7ec1bf7ac0d878c34d3fff9ebee42
박상훈 <psh950@naver.com>
2023-06-15 09:58:30 +0900

    하던거

--- thejava8/section3/App.java
+++ thejava8/section3/App.java
@@ -1,6 +1,9 @@
 package thejava8.section3;
 
+import javax.sql.rowset.Predicate;
 import java.util.*;
+import java.util.stream.Collectors;
+
 public class App {
     public static void main(String[] args) {
         List<OnlineClass> springClasses = new ArrayList<>();
@@ -9,6 +12,22 @@ public class App {
         springClasses.add(new OnlineClass(3, "spring mvc", false));
         springClasses.add(new OnlineClass(4, "spring core", false));
         springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        System.out.println("Spring 으로 시작하는 수업");
+        springClasses.stream()
+                .filter(s -> s.getTitle().startsWith("Spring"))  // 중개 오퍼레이터
+                .forEach(x -> System.out.println(x.getId()))  // 종료 오퍼레이터 void
+        ;
+
+        System.out.println("close 되지 않는 수업");
+        springClasses.stream()
+                // .filter(s -> !s.isClosed())
+                .filter(Predicate.not(OnlineClass::isClosed))  // method reference 에 not 을 넣을 수 없음
+                .forEach(s -> System.out.println(s.getId()))
+        ;
+
+        System.out.println("수업 이름만 모아서 스트림 만들기");
+        // todo
 
         List<OnlineClass> javaClasses = new ArrayList<>();
         javaClasses.add(new OnlineClass(6, "The Java, Test", true));
@@ -19,12 +38,6 @@ public class App {
         keesunEvents.add(springClasses);
         keesunEvents.add(javaClasses);
 
-        System.out.println("Spring 으로 시작하는 수업");
-        // todo
-        System.out.println("close 되지 않는 수업");
-        // todo
-        System.out.println("수업 이름만 모아서 스트림 만들기");
-        // todo
         System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
         // todo
         System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
